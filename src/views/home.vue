b2c992f8fb4dc1c1bf5b30fcf8e4bfa89db531cd
xiaofan10 <xiaoruiaini88@163.com>
2024-08-23 11:26:19 +0800

    feat:init vue 模版

---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {};
+  },
+  created() {
+    console.log("home created");
+  },
+  mounted() {
+    console.log("home mounted");
+  },
+};
+</script>
+<style scoped lang="less">
+@color: rgb(112, 222, 141);
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: @color;
+}
+</style>
